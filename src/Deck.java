cb350feaaf8e284849aef0ec42c6e26acec74501
Matthew Williams (maw101) <maw101@aber.ac.uk>
2019-06-24 13:59:00 +0100

    Create Deck.java

---
+++ src/Deck.java
@@ -0,0 +1,90 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * The type Deck - represents a deck of playing cards.
+ *
+ * @author  Matthew Williams
+ * @version 1.0
+ * @since   2019-06-24
+ */
+public class Deck {
+
+    private Card[] deck;
+    private int numOfCardsDealt;
+
+    /**
+     * Instantiates a new Deck.
+     */
+    public Deck() {
+        this(false);
+    }
+
+    /**
+     * Instantiates a new Deck.
+     *
+     * @param addJokers the add jokers
+     */
+    public Deck(boolean addJokers) {
+        int cardCounter = 0;
+        numOfCardsDealt = 0;
+
+        if (addJokers) {
+            deck = new Card[54];
+            deck[52] = new Card(1, Card.JOKER);
+            deck[53] = new Card(2, Card.JOKER);
+        } else {
+            deck = new Card[52];
+        }
+
+        for (int suit = 0; suit < 4; suit++)
+            for (int rank = 1; rank <= Card.KING; rank++)
+                deck[cardCounter++] = new Card(suit, rank);
+    }
+
+    /**
+     * Deal card card.
+     *
+     * @return the card
+     */
+    public Card dealCard() {
+        if (numOfCardsDealt == deck.length)
+            throw new IllegalStateException("No Cards Remaining in Deck");
+        return deck[numOfCardsDealt++];
+    }
+
+    /**
+     * Shuffle.
+     */
+    public void shuffle() {
+        numOfCardsDealt = 0;
+        // Fisher-Yates shuffle:
+        Random rand = ThreadLocalRandom.current();
+        for (int i = deck.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            // Simple swap
+            Card temp = deck[index];
+            deck[index] = deck[i];
+            deck[i] = temp;
+        }
+    }
+
+    /**
+     * Gets number of cards remaining.
+     *
+     * @return the number of cards remaining
+     */
+    public int getNumberOfCardsRemaining() {
+        return deck.length - numOfCardsDealt;
+    }
+
+    /**
+     * Contains jokers boolean.
+     *
+     * @return the boolean
+     */
+    public boolean containsJokers() {
+        return (deck.length == 54);
+    }
+
+}
